482d982d577b96441baabb8b8663753593a4cdf9
dgkans <peshwagdgdgd@gmail.com>
2024-08-25 00:28:08 -0700

    Create 01_FindDuplicate.java

---
+++ NeetCode150/01_Arrays_Hashing/01_FindDuplicate.java
@@ -0,0 +1,17 @@
+https://leetcode.com/problems/find-the-duplicate-number/description/
+
+##Code
+class Solution {
+    public int findDuplicate(int[] nums) {
+       HashSet<Integer> hs = new HashSet<Integer>();
+       for(int i=0;i<nums.length;i++)
+       {
+        if(hs.contains(nums[i]))
+            return nums[i];
+        else 
+            hs.add(nums[i]);
+       } 
+       return -1;
+    }
+}
+#end of code
